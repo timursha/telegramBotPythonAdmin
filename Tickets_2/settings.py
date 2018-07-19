a6f26925c882f17f80e7d851ac09885a81eb3bf8
timursha <timvlpn@gmail.com>
2018-07-19 03:34:11 +0300

    NOTHING

--- Tickets_2/settings.py
+++ Tickets_2/settings.py
@@ -136,3 +136,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
